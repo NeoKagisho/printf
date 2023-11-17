07fd19bed5a3fc17390fa7dfedfc0c215f97e46e
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2023-11-17 09:34:58 +0100

    printf

--- precision.c
+++ precision.c
@@ -11,68 +11,68 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int precision = -1; // Initialize precision to -1 (undefined)
+	va_list args;
+	int count = 0;
+	int precision = -1; // Initialize precision to -1 (undefined)
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%' && *(format + 1))
-        {
-            if (*(format + 1) == '.')
-            {
-                // Handle precision
-                precision = 0;
-                format += 2;
-                while (*format >= '0' && *format <= '9')
-                {
-                    precision = precision * 10 + (*format - '0');
-                    format++;
-                }
-            }
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1))
+		{
+			if (*(format + 1) == '.')
+			{
+				// Handle precision
+				precision = 0;
+				format += 2;
+				while (*format >= '0' && *format <= '9')
+				{
+					precision = precision * 10 + (*format - '0');
+					format++;
+				}
+			}
 
-            switch (*(format + 1))
-            {
-            case 'c':
-                count += write(1, &va_arg(args, int), 1);
-                break;
-            case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    if (precision >= 0)
-                    {
-                        while (*str && precision-- > 0)
-                        {
-                            count += write(1, str++, 1);
-                        }
-                    }
-                    else
-                    {
-                        count += write(1, str, _strlen(str));
-                    }
-                }
-                break;
-            case '%':
-                count += write(1, "%", 1);
-                break;
-            default:
-                count += write(1, "%", 1);
-                count += write(1, &(*(format + 1)), 1);
-            }
-            precision = -1; // Reset precision to -1 after use
-            format += 2;
-        }
-        else
-        {
-            count += write(1, format, 1);
-            format++;
-        }
-    }
+			switch (*(format + 1))
+			{
+			case 'c':
+				count += write(1, &va_arg(args, int), 1);
+				break;
+			case 's':
+			{
+				char *str = va_arg(args, char *);
+				if (precision >= 0)
+				{
+					while (*str && precision-- > 0)
+					{
+						count += write(1, str++, 1);
+					}
+				}
+				else
+				{
+					count += write(1, str, _strlen(str));
+				}
+			}
+			break;
+		case '%':
+			count += write(1, "%", 1);
+			break;
+		default:
+			count += write(1, "%", 1);
+			count += write(1, &(*(format + 1)), 1);
+		}
+		precision = -1; // Reset precision to -1 after use
+		format += 2;
+	}
+	else
+	{
+		count += write(1, format, 1);
+		format++;
+	}
+	}
 
-    va_end(args);
-    return count;
+	va_end(args);
+	return count;
 }
 
 /**
@@ -83,11 +83,11 @@ int _printf(const char *format, ...)
  */
 int _strlen(const char *str)
 {
-    int len = 0;
-    while (*str)
-    {
-        len++;
-        str++;
-    }
-    return len;
+	int len = 0;
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return len;
 }
